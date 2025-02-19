be4bd7e91802d6c265bb986c2cce2b73bcf94fbf
Caner <tuzunarcaner@outlook.com>
2025-02-19 21:43:25 +0300

    learner module updated and insert, read functions are added

--- src/learner/mod.rs
+++ src/learner/mod.rs
@@ -1,1 +1,22 @@
+use veresiye::Veresiye;
 
+use crate::proposal::{self, Proposal};
+
+#[derive(Default)]
+pub struct Learner {
+    database: Veresiye,
+}
+
+impl Learner {
+    pub fn new(database: Veresiye) -> Self {
+        Self { database }
+    }
+
+    pub fn insert(&mut self, proposal: Proposal) {
+        self.database.set(proposal.get_key(), proposal.get_value());
+    }
+
+    pub fn read(&mut self, key: String) -> Option<String> {
+        self.database.get(&key)
+    }
+}
