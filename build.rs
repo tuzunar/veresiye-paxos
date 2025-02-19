1a630a7807fb36b8dd4f5dcd204facb2b91e1359
Caner <tuzunarcaner@outlook.com>
2025-02-19 21:30:59 +0300

    protobuf and build files added

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std::error::Error;
+use std::{env, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("paxos_descriptor.bin"))
+        .compile(&["./proto/paxos.proto"], &["proto"])?;
+
+    tonic_build::compile_protos("proto/paxos.proto")?;
+
+    Ok(())
+}
