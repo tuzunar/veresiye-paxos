86e737f7a4fc41bd6ff90d4403f2e489483527df
Caner <tuzunarcaner@outlook.com>
2025-02-19 21:41:08 +0300

    new and getter methods are added

--- src/proposal/mod.rs
+++ src/proposal/mod.rs
@@ -1,11 +1,24 @@
-struct Proposal {
+#[derive(Debug, Clone, Default)]
+pub struct Proposal {
     seq_id: usize,
     key: String,
     value: String,
 }
 
 impl Proposal {
-    fn new(seq_id: usize, key: String, value: String) -> Self {
+    pub fn new(seq_id: usize, key: String, value: String) -> Self {
         Self { seq_id, key, value }
+    }
+
+    pub fn get_seq_id(&self) -> usize {
+        self.seq_id
+    }
+
+    pub fn get_key(&self) -> &str {
+        &self.key
+    }
+
+    pub fn get_value(&self) -> &str {
+        &self.value
     }
 }
