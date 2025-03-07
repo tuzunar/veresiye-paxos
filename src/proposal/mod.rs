27777d5989485f2e3029313c9796f10ee32f29a6
Caner <tuzunarcaner@outlook.com>
2025-03-07 22:45:39 +0300

    start using proposal instead seq

--- src/proposal/mod.rs
+++ src/proposal/mod.rs
@@ -1,17 +1,21 @@
 #[derive(Debug, Clone, Default)]
 pub struct Proposal {
-    seq_id: usize,
+    proposer_id: i32,
     key: String,
     value: String,
 }
 
 impl Proposal {
-    pub fn new(seq_id: usize, key: String, value: String) -> Self {
-        Self { seq_id, key, value }
+    pub fn new(proposer_id: i32, key: String, value: String) -> Self {
+        Self {
+            proposer_id,
+            key,
+            value,
+        }
     }
 
-    pub fn get_seq_id(&self) -> usize {
-        self.seq_id
+    pub fn get_proposal_id(&self) -> i32 {
+        self.proposer_id
     }
 
     pub fn get_key(&self) -> &str {
