d7fdec05b2595b78b7ae4781dc5d125e0016c11d
Caner <tuzunarcaner@outlook.com>
2025-03-15 23:57:30 +0300

    experimental arg added for proto3 syntax

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("paxos_descriptor.bin"))
         .compile_protos(&["./proto/paxos.proto"], &["proto"])?;
 
